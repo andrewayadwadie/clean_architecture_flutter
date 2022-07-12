40e138c89e17ebf55cc46369ea70144a8c28f6f9
andrewayadwadie <44525452+andrewayadwadie@users.noreply.github.com>
2022-07-12 17:22:46 +0200

    add api services and responses

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,14 @@
+import 'package:advanced_flutter_arabic/app/constants.dart';
+import 'package:advanced_flutter_arabic/data/responses/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customer/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email, @Field("password") String password);
+}
--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -22,3 +22,32 @@ class SliderViewObject {
     this.currentIndex,
   );
 }
+
+//! login Model
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotification;
+  Customer(this.id, this.name, this.numOfNotification);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+  Contacts(
+    this.phone,
+    this.email,
+    this.link,
+  );
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+  Authentication(
+    this.customer,
+    this.contacts,
+  );
+}
