a13fabfe2d991cb18dd531bc04a636d6ad725813
andrewayadwadie <44525452+andrewayadwadie@users.noreply.github.com>
2022-07-13 09:38:46 +0200

    make extension on String and int to convert null values to empty or 0 value

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -44,8 +44,8 @@ class Contacts {
 }
 
 class Authentication {
-  Customer customer;
-  Contacts contacts;
+  Customer? customer;
+  Contacts? contacts;
   Authentication(
     this.customer,
     this.contacts,
