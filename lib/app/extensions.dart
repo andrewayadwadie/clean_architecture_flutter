8921c01b0906e73cdab56faf3e193769824b3ce0
andrewayadwadie <44525452+andrewayadwadie@users.noreply.github.com>
2022-07-20 22:59:14 +0200

    make extension on String and int to convert null values to empty or 0 value

--- lib/app/extensions.dart
+++ lib/app/extensions.dart
@@ -1,7 +1,9 @@
+import 'package:advanced_flutter_arabic/app/constants.dart';
+
 extension NonNullString on String? {
   String orEmpty() {
     if (this == null) {
-      return "";
+      return Constants.empty;
     } else {
       return this!;
     }
@@ -9,9 +11,9 @@ extension NonNullString on String? {
 }
 
 extension NonNullInteger on int? {
-  int orEmpty() {
+  int orZero() {
     if (this == null) {
-      return 0;
+      return Constants.zero;
     } else {
       return this!;
     }
